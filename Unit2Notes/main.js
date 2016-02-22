1d32b2636d080527058ce93c9c253f31d0e100e7
Dipty Singhania <dsinghania@yahoo.com>
2016-02-22 13:00:04 -0600

    Unit2Notes

---
+++ Unit2Notes/main.js
@@ -0,0 +1,21 @@
+
+console.log("Hello World!");
+
+// single line comment
+var foo = "bar with a 'quote' and another \"quote\"";
+var foo2 = 'bar with a "quote"';
+
+var my_num = 2.0;
+
+/* 
+	multi-line comment
+*/
+
+var something = 3;
+
+var MyCar
+
+console.log("hey there");
+
+console.log("2 plus 2 equals: " + (2+2));
+//2 plus 2 equals: 4
