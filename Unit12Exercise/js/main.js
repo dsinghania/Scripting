0c43f8cd6b3891c7ca31cf045754bcdb2b51665f
Dipty Singhania <dsinghania@yahoo.com>
2016-03-14 10:32:40 -0500

    Unit12Exercise

--- Unit12Exercise/js/main.js
+++ Unit12Exercise/js/main.js
@@ -2,6 +2,8 @@
 $(document).ready(function(){
     
     console.log("hello world!");
+    
+    $(".done").remove();
     var url = "http://parkland-csc175.github.io/csc175data/todos/list.json";
     
     $.get(url, function(result){
@@ -9,7 +11,7 @@ $(document).ready(function(){
         
        result.forEach(function(post){
            
-           var $li = $("<li></li>").text(post.job);
+           var $li = $("<li></li>").text(post.job); //creates a li element 
            if(post.done)
                $li.addClass("done");    
            $("body ul").append($li);       
