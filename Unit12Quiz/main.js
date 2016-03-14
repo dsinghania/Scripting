26a9bc031947ea85437c01b6b5e8a41e8942931f
Dipty Singhania <dsinghania@yahoo.com>
2016-03-14 10:21:20 -0500

    Unit12Quiz

--- Unit12Quiz/main.js
+++ Unit12Quiz/main.js
@@ -3,12 +3,13 @@ console.log("Hello World!");
 
 var items = ["I", "passed", "the", "quiz"];
 
-var divWrapper = document.getElementById("#wrapper");
+var divWrapper = document.getElementById("wrapper");
 
 for(var i = 0; i < items.length; i++)
 {
     var pElement = document.createElement("p");
     pElement.className = "quiz-word";
     pElement.innerHTML = items[i];
+    //console.log(items[i]);
     divWrapper.appendChild(pElement);
 }
