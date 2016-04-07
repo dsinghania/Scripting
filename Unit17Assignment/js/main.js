48c10fe40ff4e8dabdf2effca6ad751e57022f9e
Dipty Singhania <dsinghania@yahoo.com>
2016-04-07 13:01:38 -0500

    Updated Unit 17 Assignment

--- Unit17Assignment/js/main.js
+++ Unit17Assignment/js/main.js
@@ -1,48 +1,37 @@
 // https://learn.jquery.com/using-jquery-core/document-ready/
-// Crud Interfaces
+// Crud Interfaces  Create Retrieve Update & Delete
 
-$(document).ready(function(){
-    
-    console.log("hello world");
+$(document).ready(function(){    
+    var todos = [];     //an array to hold all the input values
     var inputValue;
+    var rowId;
+        
+    getToDos();
+    listTodos();
+    //console.log(todos);
+      
     $("#btn-todo-add").click(function(){
         
-        inputValue = document.forms[0].elements["task"].value;
-        localStorage.setItem("added" , inputValue);
-        //console.log(inputValue);
+        //inputValue = document.forms[0].elements["task"].value;
+        inputValue = $("#todo-text").val();
         
-        var $tab = $(".table");
-        var $tr =  $("<tr></tr>");
-        $tr.html = inputValue;
-        $tab.append($tr);
+        if(inputValue == ""){
+            alert("To-do List is empty");
+            return false;
+        }
+        addNewTodo(inputValue);  
+        createRow(inputValue);
+        saveTodos();
+        location.reload();      //reloads the resource from the current URL.
+    });
         
-        var $td1 =  $("<td></td");
-        $td1.html(inputValue);
-        $tr.append($td1);
-        
-        var $td2 = $("<td></td");
-        var $button1 = $("<button></button>").addClass("btn btn-icon").attr("data-action", "complete").attr("title", "complete");
-        $button1.append($('<span></span>') .addClass('glyphicon glyphicon-ok'));
-        $td2.append($button1);
-        
-        var $button2 = $("<button></button>").addClass("btn btn-icon").attr("data-action", "remove").attr("title", "remove");
-        $button2.append($('<span></span>') .addClass('glyphicon glyphicon-trash'));
-        $td2.append($button2);
-        
-        $tr.append($td2);
-        
-        var $td3 =  $("<td></td").append($("<span></span>").addClass("completed-ticker"));
-        
-        $tr.append($td3);
-        localStorage.setItem("tableAdded", $tab);
-        
-    $("button[data-action='complete']").click(function(e){
-        localStorage.setItem("completed" , inputValue);
-            
+    $("button[data-action='complete']").click(function(){        
+           
         var $row = $(this).closest('tr'); 
         $row.addClass("completed");
-        //$(this).css("visibility", "visible");
-           
+        
+        rowId = $row.attr("id");
+        
         // $(".alert-success").css("display", "block");
         $(".alert-success").show();
             
@@ -50,11 +39,7 @@ $(document).ready(function(){
             $(".alert-success").hide();                
         }
         setTimeout(hideMessage, 3 * 1000);
-            
-        var dt = new Date();
-        var time1 = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-        //console.log(time1);
-        
+                    
         var diff = 0;
         function timeDisplay(){
             var output = "Completed " ;
@@ -64,34 +49,98 @@ $(document).ready(function(){
             $row.find("td:last-child").html(output);
             diff++;
         }
-        setTimeout(timeDisplay, 1);
-        setInterval(timeDisplay, 60 * 1000);           
-           
+        setTimeout(timeDisplay, 0);
+        setInterval(timeDisplay, 60 * 1000);
+        
+        var updateIndex = findIndex(rowId);
+        
+        todos[updateIndex].completed = true;        
+        saveTodos();         
+        //location.reload();    //removes away the strike through from the input and also the time display  
     });
-    /*
-    var url = "file:///C:/Users/Dipty/Desktop/CSC175/Unit17Assignment/index.html";
-    url += "?task=";
-    var arr = inputValue.split(" ");
-    for (var i = 0; i < arr.length; i++) {
-        url += arr[i] + "+";
-    }
-    document.location.assign(url);
-    */
-    var input = localStorage.getItem("added");
-    var tab = localStorage.getItem("tableAdded");
     
-        
-    $("button[data-action='remove']").click(function(e){
-        
+    $("button[data-action='remove']").click(function(){
+            
         var $row = $(this).closest('tr'); 
-        $row.remove();
-        //localStorage.removeItem("added");
-        //localstorage.removeItem("completed");
-    });
- 
-    });
-});
+        rowId = $row.attr("id"); 
+        var removeIndex = findIndex(rowId);
+        todos.splice(removeIndex, 1);
+        saveTodos();
+        location.reload();          //reload the current document
+        $row.remove(); 
+    }); 
     
-    $(window).bind("storage", function(){
+    function createRow(inputValue){
+        var $tab = $(".table");
+        var $tr =  $("<tr></tr>").attr("id", inputValue);
+
+        var $td1 =  $("<td></td");
+        $td1.html(inputValue);
+        
+        var $td2 = $("<td></td");
+        
+        var $button1 = $("<button></button>").addClass("btn btn-icon").attr("data-action", "complete").attr("title", "complete");
+        $button1.append($('<span></span>') .addClass('glyphicon glyphicon-ok'));
+        
+        var $button2 = $("<button></button>").addClass("btn btn-icon").attr("data-action", "remove").attr("title", "remove");
+        $button2.append($('<span></span>') .addClass('glyphicon glyphicon-trash'));
+        
+        $td2.append($button1);
+        $td2.append($button2);        
+    
+        var $td3 =  $("<td></td").append($("<span></span>").addClass("completed-ticker"));
+        
+        $tr.append($td1);
+        $tr.append($td2);
+        $tr.append($td3);
+        
+        $tab.append($tr);
+    }
+    
+    //https://www.youtube.com/watch?v=e3XtHxV_fck
+    
+    function addNewTodo(input){        
+        var todo = {
+            name:      input,
+            completed: false
+        };
+        todos.push(todo);        
+    }
+    
+    function saveTodos(){        
+        var str = JSON.stringify(todos);
+        localStorage.setItem("todos", str);
+    }
+    
+    function getToDos(){
+        var str = localStorage.getItem("todos");
+        todos = JSON.parse(str);
+        if(!todos)
+            todos = [];        
+    }
+    
+    function listTodos(){
+        for(var i = 0; i < todos.length; i++){
+            var todo =  todos[i];
+            var input = todo.name;
+            createRow(input);
+        }
+    }    
+    
+    function findIndex(rowId){   
+        var index;     
+        for(var i = 0; i < todos.length; i++){
+            var todo = todos[i];
+            if(todo.name === rowId){
+                index = i;
+                break;
+            }
+        } 
+        return index;
+    }
+});
+
+    
+   /* $(window).bind("storage", function(){
        alert("something changed!") 
-    });
+    });*/
