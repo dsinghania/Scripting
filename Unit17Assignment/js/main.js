66f5bfdb930e62cef7bcab36fcfe38df9799e87a
Dipty Singhania <dsinghania@yahoo.com>
2016-04-07 17:03:41 -0500

    Unit 17 Assignment

--- Unit17Assignment/js/main.js
+++ Unit17Assignment/js/main.js
@@ -19,43 +19,21 @@ $(document).ready(function(){
             alert("To-do List is empty");
             return false;
         }
-        addNewTodo(inputValue);  
-        createRow(inputValue);
+        var todo = addNewTodo(inputValue);
+        createRow(todo);
         saveTodos();
-        location.reload();      //reloads the resource from the current URL.
+        //location.reload();      //reloads the resource from the current URL.
     });
         
     $("button[data-action='complete']").click(function(){        
            
-        var $row = $(this).closest('tr'); 
-        $row.addClass("completed");
+        var $row = $(this).closest('tr');        
+        showCompleted($row);
         
-        rowId = $row.attr("id");
-        
-        // $(".alert-success").css("display", "block");
-        $(".alert-success").show();
-            
-        function hideMessage(){
-            $(".alert-success").hide();                
-        }
-        setTimeout(hideMessage, 3 * 1000);
-                    
-        var diff = 0;
-        function timeDisplay(){
-            var output = "Completed " ;
-            output += diff;
-            output += diff < 2 ? " minute" : "minutes";
-            output += " ago.";
-            $row.find("td:last-child").html(output);
-            diff++;
-        }
-        setTimeout(timeDisplay, 0);
-        setInterval(timeDisplay, 60 * 1000);
-        
-        var updateIndex = findIndex(rowId);
-        
+        rowId = $row.attr("id");        
+        var updateIndex = findIndex(rowId);        
         todos[updateIndex].completed = true;        
-        saveTodos();         
+        saveTodos();       
         //location.reload();    //removes away the strike through from the input and also the time display  
     });
     
@@ -66,35 +44,41 @@ $(document).ready(function(){
         var removeIndex = findIndex(rowId);
         todos.splice(removeIndex, 1);
         saveTodos();
-        location.reload();          //reload the current document
+        //location.reload();          //reload the current document
         $row.remove(); 
     }); 
     
-    function createRow(inputValue){
+    function createRow(todo){
+        var inputValue = todo.name;
+        var complete = todo.completed;
+        
         var $tab = $(".table");
         var $tr =  $("<tr></tr>").attr("id", inputValue);
 
         var $td1 =  $("<td></td");
         $td1.html(inputValue);
-        
+            
         var $td2 = $("<td></td");
-        
+            
         var $button1 = $("<button></button>").addClass("btn btn-icon").attr("data-action", "complete").attr("title", "complete");
         $button1.append($('<span></span>') .addClass('glyphicon glyphicon-ok'));
-        
+            
         var $button2 = $("<button></button>").addClass("btn btn-icon").attr("data-action", "remove").attr("title", "remove");
         $button2.append($('<span></span>') .addClass('glyphicon glyphicon-trash'));
-        
+           
         $td2.append($button1);
         $td2.append($button2);        
-    
-        var $td3 =  $("<td></td").append($("<span></span>").addClass("completed-ticker"));
         
+        var $td3 =  $("<td></td").append($("<span></span>").addClass("completed-ticker"));
+            
         $tr.append($td1);
         $tr.append($td2);
         $tr.append($td3);
-        
+            
         $tab.append($tr);
+        
+        if(complete)
+            showCompleted($tr);
     }
     
     //https://www.youtube.com/watch?v=e3XtHxV_fck
@@ -104,7 +88,8 @@ $(document).ready(function(){
             name:      input,
             completed: false
         };
-        todos.push(todo);        
+        todos.push(todo);  
+        return todo;      
     }
     
     function saveTodos(){        
@@ -121,9 +106,7 @@ $(document).ready(function(){
     
     function listTodos(){
         for(var i = 0; i < todos.length; i++){
-            var todo =  todos[i];
-            var input = todo.name;
-            createRow(input);
+            createRow(todos[i]);
         }
     }    
     
@@ -138,8 +121,31 @@ $(document).ready(function(){
         } 
         return index;
     }
+    
+    function showCompleted($row){         
+        $row.addClass("completed");
+        
+        // $(".alert-success").css("display", "block");
+        $(".alert-success").show();
+            
+        function hideMessage(){
+            $(".alert-success").hide();                
+        }
+        setTimeout(hideMessage, 3 * 1000);
+                    
+        var diff = 0;
+        function timeDisplay(){
+            var output = "Completed " ;
+            output += diff;
+            output += diff < 2 ? " minute" : " minutes";
+            output += " ago.";
+            $row.find("td:last-child").html(output);
+            diff++;
+        }
+        setTimeout(timeDisplay, 0);
+        setInterval(timeDisplay, 60 * 1000);
+    }
 });
-
     
    /* $(window).bind("storage", function(){
        alert("something changed!") 
