705639b7cec67f0472ab8970317d6be05b379d54
Dipty Singhania <dsinghania@yahoo.com>
2016-04-12 11:16:45 -0500

    Updated Unit 18 Exercise

--- Unit18Exercise/Unit18Exercise/main.js
+++ Unit18Exercise/Unit18Exercise/main.js
@@ -4,30 +4,18 @@ $(document).ready(function(){
     var url = "https://parkland-CSC175.github.io/csc175data/cars/car-makes.json";
     
     $.get(url, function(result){
-         //console.log(result);
          var viewModel = {
              carBrands:    ko.observableArray(),
              brandToAdd:   ko.observable(""),
              countryToAdd: ko.observable(""),
              addCarBrands: function(){
-                // brandInputValue = $("#brand-text").val();
-                 //countryInputValue = $("#country-text").val();
-                 if(brandToAdd() != "" && countryToAdd != ""){
+                 if(brandToAdd != "" && countryToAdd != ""){
                      console.log(brand + " " + countryToAdd);
                      carBrands.push({
                          brand:   brandToAdd,
                          country: countryToAdd
                      })
                  }
-                  /*
-                if(brandInputValue == "" || countryInputValue == ""){
-                     alert("Brand or Country input field is empty");
-                     return false;
-                 }                     
-                viewModel.carBrands.push({
-                     brand: brandInputValue,
-                     country: countryInputValue                        
-                 })*/
              },
              removeRow: function(){
                  viewModel.carBrands.remove(this);
@@ -46,3 +34,16 @@ $(document).ready(function(){
              ko.applyBindings(viewModel);
     });
 });   
+
+                // brandInputValue = $("#brand-text").val();
+                 //countryInputValue = $("#country-text").val();
+                 
+                  /*
+                if(brandInputValue == "" || countryInputValue == ""){
+                     alert("Brand or Country input field is empty");
+                     return false;
+                 }                     
+                viewModel.carBrands.push({
+                     brand: brandInputValue,
+                     country: countryInputValue                        
+                 })*/
