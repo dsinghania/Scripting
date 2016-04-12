553e83c3fd75f43630675ff93da488f867e4dacb
Dipty Singhania <dsinghania@yahoo.com>
2016-04-12 11:23:37 -0500

    Unit 18 Exercise

--- Unit18Exercise/main.js
+++ Unit18Exercise/main.js
@@ -1,42 +1,49 @@
 // https://learn.jquery.com/using-jquery-core/document-ready/
 $(document).ready(function(){
-    
-    console.log("hello world");
 
     var url = "https://parkland-CSC175.github.io/csc175data/cars/car-makes.json";
     
-     $.get(url, function(result){
-         console.log(result);
-         var viewModel;       
-         result.forEach(function(post){
-             console.log(post);
-             console.log(post.make_display);
-             viewModel = {
-                 carBrands: ko.observable([]), 
-                 setJsonData: function(){
-                     this.carBrands.push({
-                         brand: post.make_display,
-                         country: post.make_country                        
-                     })
-                 },               
-                 addCarBrands: function(){
-                     brandInputValue = $("#brand-text").val();
-                     countryInputValue = $("#country-text").val();
-                    
-                     if(brandInputValue == "" || countryInputValue == ""){
-                         alert("Brand or Country input field is empty");
-                         return false;
-                     }
-                     this.carBrands.push({
-                         brand: brandInputValue,
-                         country: countryInputValue                        
+    $.get(url, function(result){
+         var viewModel = {
+             carBrands:    ko.observableArray(),
+             brandToAdd:   ko.observable(""),
+             countryToAdd: ko.observable(""),
+             addCarBrands: function(){
+                 if(brandToAdd != "" && countryToAdd != ""){
+                     console.log(brand + " " + countryToAdd);
+                     carBrands.push({
+                         brand:   brandToAdd,
+                         country: countryToAdd
                      })
                  }
-             };
-            //viewModel.carBrands.sort();
-         });
-            //console.log(viewModel.carBrands);
+             },
+             removeRow: function(){
+                 viewModel.carBrands.remove(this);
+             },
+             completed: false
+         };
+                     
+         result.forEach(function(post){
+            viewModel.carBrands.push({
+                brand:   post.make_display,
+                country: post.make_country                        
+            });
+        }); 
+            viewModel.carBrands.sort();        
             //links the viewModel to the html
              ko.applyBindings(viewModel);
     });
-});
+});   
+
+                // brandInputValue = $("#brand-text").val();
+                 //countryInputValue = $("#country-text").val();
+                 
+                  /*
+                if(brandInputValue == "" || countryInputValue == ""){
+                     alert("Brand or Country input field is empty");
+                     return false;
+                 }                     
+                viewModel.carBrands.push({
+                     brand: brandInputValue,
+                     country: countryInputValue                        
+                 })*/
