d697df727464f47ada0bc426886c76d0390eab61
Dipty Singhania <dsinghania@yahoo.com>
2016-04-13 14:08:51 -0500

    Unit 18 exercise

--- Unit18Exercise/main.js
+++ Unit18Exercise/main.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     var url = "https://parkland-CSC175.github.io/csc175data/cars/car-makes.json";
     
     $.get(url, function(result){
+        
          var viewModel = {
              carBrands:    ko.observableArray([]),
              brandToAdd:   ko.observable(""),
@@ -16,22 +17,29 @@ $(document).ready(function(){
                          country: this.countryToAdd()
                      });
                  } else {
-                     alert("One of the input fields is empty");
+                     alert("One of the input field is empty");
                      return false;
-                 } 
+                 }   
+                 this.sortBrands();
                  
-                 //this.sortBrands();
-                                  
-                 this.brandToAdd("");       // Clear the text box           
+                 this.brandToAdd("");       // Clear the text box  
+                          
                  this.countryToAdd("");                               
              },
-             /*
              sortBrands: function() {
-                 this.carBrands.sort(function(a,b){
-                     return a.brand > b.brand ? 1 : -1;
-                 });                    
-             },  
-             */           
+                 this.carBrands.sort(function(l, r) {
+                     var lBrand = l.brand.toLowerCase();
+                     var rBrand = r.brand.toLowerCase();
+                     if (lBrand == rBrand)
+                         return 0;
+
+                     if (lBrand > rBrand)
+                        return 1;
+
+                     if (lBrand < rBrand)
+                        return -1;
+                 });                   
+             },           
              removeRow: function(){
                  viewModel.carBrands.remove(this);
              }
@@ -42,8 +50,10 @@ $(document).ready(function(){
                  brand:   post.make_display,
                  country: post.make_country                        
              });
-        }); 
-        //viewModel.carBrands().sort();    
+        });
+         
+        viewModel.sortBrands();    
+        
         //links the viewModel to the html
         ko.applyBindings(viewModel);
     });
